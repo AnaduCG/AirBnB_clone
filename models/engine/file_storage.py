c03e1fd499512d92b6a3449662e3f0252bd98964
Kariaki58 <Kariakichernocherno@gmail.com>
2023-08-08 10:52:50 +0100

    I have not finish with implementing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,2 +1,20 @@
+import json
+from models.base_model import BaseModel
+
 class FileStorage:
-    ...
+    __file_path = "file.json"
+    __objects = {}
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        string = f"{obj.__class__.__name__}.{str(obj)}"
+        self.__objects[string] = obj
+
+    def save(self):
+        temp_storage = []
+        with open(file=self.__file_path, mode="a", encoding="UTF8") as file:
+            json.dump(file, self.__objects)
+
+    def reload(self):
+        ...   
