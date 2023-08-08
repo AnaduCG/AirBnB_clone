3d7c64a00b7a6a76ee7ae9a840ddf4b51bca7f1f
AnaduCG <godzionwin@gmail.com>
2023-08-08 11:17:20 +0100

    Pycodestyling: Fixing pycodestyle formatting

--- models/base_model.py
+++ models/base_model.py
@@ -22,15 +22,15 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        
+
     def __str__(self):
         """returns string representation of the object"""
         return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
         """updating time"""
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         """return a dictionary representation"""
         self.created_at = datetime.isoformat(self.created_at)
@@ -38,4 +38,3 @@ class BaseModel:
         inst_dict = self.__dict__
         inst_dict["__class__"] = __class__.__name__
         return inst_dict
-    
