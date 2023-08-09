7b97a3aa4eb43cef534d2aff6a21ed352dc6090c
AnaduCG <godzionwin@gmail.com>
2023-08-09 12:37:55 +0100

    Feature: implemeted data storage in json format

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ datetime - get the current day and time
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -22,6 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """returns string representation of the object"""
@@ -30,6 +32,7 @@ class BaseModel:
     def save(self):
         """updating time"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """return a dictionary representation"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,2 +1,34 @@
+#!/usr/bin/python3
+import json
+import os
+
+
 class FileStorage:
-    ...
+    """ Class that handles storing data in json format """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ Method that returns the dictionary (__objects) """
+        return self.__objects
+
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id """
+        key = f"{obj.__class__.__name__}.{str(obj.id)}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """ serializes __objects to the JSON file (path: __file_path) """
+        json_data = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_data, f)
+
+    def reload(self):
+        """  deserializes the JSON file to __objects (only if the JSON file """
+        from models.base_model import BaseModel
+
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, 'r') as f:
+                object_data = json.load(f)
+                for key, value in object_data.items():
+                    self.__objects[key] = eval(value['__class__'])(**value)
