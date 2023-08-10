3f5f52bb5d51caa18db3ca7957baee9619005b23
Kariaki58 <Kariakichernocherno@gmail.com>
2023-08-10 22:37:29 +0100

    json file is behaving weirdly

--- console.py
+++ console.py
@@ -1,133 +1,135 @@
 import cmd
-import models.base_model as model
+from models.base_model import BaseModel
 import models
 import json
 
 
 class HBNBCommand(cmd.Cmd):
-    """hbnb command line"""
-
     prompt = "(hbnb) "
-    class_str_names = ["BaseModel"]
+    class_names = {'BaseModel': BaseModel}
+
     def do_quit(self, other):
         """Quit command to exit the program"""
         return True
-    
+
     def do_EOF(self, other):
-        """Exist the Program"""
+        """Quit command to exit the program"""
         return True
-    
-    def emptyline(self):
+
+    def emptyline(self) -> bool:
+        """emptyline"""
         ...
-        
+    
+    def validate(self, other):
+        """validate other input"""
+        list_paras = other.split(" ")
+        key_list = list(HBNBCommand.class_names.keys())
+        if list_paras[0] not in key_list[0]:
+            print("** class doesn't exist **")
+            return False
+        else:
+            return True
+    
     def do_create(self, other):
         """create (className)"""
-        list_other = other.split(" ")
         if other:
-            dummy_inst = model.BaseModel()
-            class_name = dummy_inst.__class__.__name__
-            if class_name == list_other[0]:
-                dummy_inst.save()
-                print(dummy_inst.id)
-            else:
-                print("** class doesn't exit **")
+            if self.validate(other):
+                new_insts = list(HBNBCommand.class_names.values())[0]()
+                print(new_insts.id)
         else:
             print("** class name missing **")
-        
+    
     def do_show(self, other):
         """show (className)"""
-        list_other = other.split(" ")
         if other:
-            class_name = model.BaseModel().__class__.__name__
-            if class_name != list_other[0]:
-                print("** class doesn't exist **")
-            elif len(list_other) == 1:
-                print("** instance id missing **")
-            else:
-                models.storage.reload()
-                load_dict = models.storage.all()
-                dict_text = f"{class_name}.{list_other[1]}"
-                if dict_text in load_dict:
-                    get_id_dict = load_dict[dict_text]
-                    print(get_id_dict)
+            if self.validate(other):
+                list_other = other.split()
+                if len(list_other) == 1:
+                    print("** instance id missing **")
                 else:
-                    print("** no instance found **")
+                    models.storage.reload()
+                    key_list = list(HBNBCommand.class_names.keys())
+                    load_dict = models.storage.all()
+                    dict_text = f"{key_list[0]}.{list_other[1]}"
+                    if dict_text in load_dict:
+                        get_id_dict = load_dict[dict_text]
+                        print(get_id_dict)
+                    else:
+                        print("** no instance found **")
         else:
             print("** class name missing **")
     
     def do_destroy(self, other):
-        """destory (ClassName)"""
-        list_other = other.split(" ")
+        """destroy (className)"""
         if other:
-            class_name = model.BaseModel().__class__.__name__
-            if class_name != list_other[0]:
-                print("** class doesn't exist **")
-            elif len(list_other) == 1:
-                print("** instance id missing **")
-            else:
-                models.storage.reload()
-                load_dict = models.storage.all()
-                dict_text = f"{class_name}.{list_other[1]}"
-                if dict_text in load_dict:
-                    get_id_dict = load_dict[dict_text]
-                    del get_id_dict
-                    models.storage.save()
+            if self.validate(other):
+                list_other = other.split(" ")
+                if (len(list_other) == 1):
+                    print("** instance id missing **")
                 else:
-                    print("** no instance found **")
+                    models.storage.reload()
+                    load_dict = models.storage.all()
+                    key_list = list(HBNBCommand.class_names.keys())
+                    dict_text = f"{key_list[0]}.{list_other[1]}"
+                    if dict_text in list(load_dict.keys()):
+                        del load_dict[dict_text]
+                    else:
+
+                        print("** no instance found **")
+                    models.storage.save()
         else:
             print("** class name missing **")
     
     def do_all(self, other):
         """all (ClassName)"""
-        models.storage.reload()
-        JSONLIST = []
-        load_dict = models.storage.all()
-        if not other:
-            for key in load_dict:
-                JSONLIST.append(str(load_dict[key]))
-            print(json.dumps(JSONLIST))
-            return
-        list_other = other.split(" ")
-        class_name = "BaseModel"
-        if list_other[0] == class_name:
-            for key in load_dict:
-                if list_other[0] in key:
-                    JSONLIST.append(str(load_dict[key]))
-            print(json.dumps(JSONLIST))
-        else:
-            print("** class doesn't exist **")
-    
-    def do_update(self, other):
-        """update (ClassName)"""
         if other:
-            list_other = other.split(" ")
-            print(list_other)
-            if list_other[0] != "BaseModel":
-                print("** class doesn't exist **")
-                return
-            elif len(list_other) == 1:
-                print("** instance id missing **")
-                return
-            class_name = "BaseModel"
             models.storage.reload()
+            JSONLIST = []
             load_dict = models.storage.all()
-            dict_text = f"{class_name}.{list_other[1]}"
-            if dict_text not in load_dict:
-                print("** no instance found **")
-            elif len(list_other) == 2:
-                print("** attribute name missing **")
-            elif len(list_other) == 3:
-                print("** attribute name missing **")
-            else:
-                dict_data = load_dict[dict_text]
-                change_text = list_other[3]
-                no_quote = change_text.strip('"').replace('\\"', '"')
-                if hasattr(dict_data, list_other[2]):
-                    json_type = type(getattr(dict_data, list_other[2]))
-                    setattr(dict_data, list_other[2], json_type(no_quote))
+            if other:
+                list_other = other.split(" ")
+                class_name = "BaseModel"
+                if list_other[0] == class_name:
+                    for key in load_dict:
+                        if list_other[0] in key:
+                            JSONLIST.append(str(load_dict[key]))
+                    print(json.dumps(JSONLIST))
                 else:
-                    setattr(dict_data, list_other[2], no_quote)
-                models.storage.save()
+                    print("** class doesn't exist **")
+            else:
+                for key in load_dict:
+                    JSONLIST.append(str(load_dict[key]))
+                print(json.dumps(JSONLIST))
+        else:
+            print("** class name missing **")
+    def do_update(self, other):
+        """update (className)"""
+        if other:
+            if self.validate(other):
+                list_other = other.split()
+                if (len(list_other) == 1):
+                    print("** instance id missing **")
+                else:
+                    models.storage.reload()
+                    load_dict = models.storage.all()#data in json file
+                    key_list = list(HBNBCommand.class_names.keys())
+                    dict_text = f"{key_list[0]}.{list_other[1]}"
+                if dict_text not in load_dict:
+                    print("** no instance found **")
+                elif len(list_other) == 2:
+                    print("** attribute name missing **")
+                elif len(list_other) == 3:
+                    print("** value missing **")
+                else:
+                    dict_data = load_dict[dict_text]
+                    change_text = list_other[3]
+                    no_quote = change_text.strip('"').replace('\\"', '"')
+                    if hasattr(dict_data, list_other[2]):
+                        json_type = type(getattr(dict_data, list_other[2]))
+                        setattr(dict_data, list_other[2], json_type(no_quote))
+                    else:
+                        setattr(dict_data, list_other[2], no_quote)
+                    models.storage.save()
         else:
             print("** class name missing **")
 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,32 +1,40 @@
-#!/usr/bin/python3
 import json
+from models.base_model import BaseModel
 
 
 class FileStorage:
     __file_path = "file.json"
-    __object = {}
-
+    __objects = {}
     def all(self):
-        return self.__object
-
+        """return object of dictionary"""
+        return self.__objects
+    
     def new(self, obj):
-        self.__object[obj.__class__.__name__ + '.' + str(obj.id)] = obj
+        """create a new object
 
+        Args:
+            obj (obj): instance of object
+        """
+        class_key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[class_key] = obj
+    
     def save(self):
+        """serilize object to json file"""
         dummy_dict = {}
-        empty_string = ""
-        for key, value in self.__object.items():
+        for key, value in self.__objects.items():
             dummy_dict[key] = value.to_dict()
-        empty_string = json.dumps(dummy_dict)
         with open(self.__file_path, mode="w", encoding="UTF8") as file:
-            file.write(empty_string)
+            json.dump(dummy_dict, file)
     
     def reload(self):
+        """deserilize object from json file"""
         try:
             with open(self.__file_path, mode="r", encoding="UTF8") as file:
                 json_data = json.load(file)
             for key, value in json_data.items():
-                self.new(BaseModel(**value))
+                class_name = value["__class__"]
+                del value["__class__"]
+                obj = eval(class_name)(**value)
+                self.__objects[key] = obj
         except Exception:
-            return
-          
+            ...
