fbf1e6b393287b613fca832e0f81e45f2ed7285d
Kariaki58 <Kariakichernocherno@gmail.com>
2023-08-11 14:07:58 +0100

    code tested and still under test, done with console, finish to task 10

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,11 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -34,8 +39,19 @@ class FileStorage:
                 json_data = json.load(file)
             for key, value in json_data.items():
                 class_name = value["__class__"]
-                del value["__class__"]
-                obj = eval(class_name)(**value)
-                self.__objects[key] = obj
+                if class_name == "BaseModel":
+                    self.__objects[key] = BaseModel(**value)
+                if class_name == "User":
+                    self.__objects[key] = User(**value)
+                if class_name == "Place":
+                    self.__objects[key] = Place(**value)
+                if class_name == "State":
+                    self.__objects[key] = State(**value)
+                if class_name == "City":
+                    self.__objects[key] = City(**value)
+                if class_name == "Amenity":
+                    self.__objects[key] = Amenity(**value)
+                if class_name == "Review":
+                    self.__objects[key] = Review(**value)
         except Exception:
             pass
---
+++ models/state.py
@@ -0,0 +1,7 @@
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    def __init__(self, *args, **kwargs):
+        self.name = ""
+        super().__init__(*args, **kwargs)
